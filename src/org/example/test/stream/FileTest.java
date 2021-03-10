a7deb777dc3d11a522e2b03bc28598d906ec45f8
su <fiush>
2021-03-10 21:09:21 +0800

    新增File测试类

---
+++ src/org/example/test/stream/FileTest.java
@@ -0,0 +1,9 @@
+package org.example.test.stream;
+
+import java.io.File;
+
+public class FileTest {
+    public static void main(String[] args) {
+        File file = new File("/Users/su/IdeaProjects/JavaSource");
+    }
+}
